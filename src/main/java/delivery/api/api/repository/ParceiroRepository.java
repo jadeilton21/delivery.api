4fa896a64751dd29a141483f91be2741b3c6dc43
delivery.api <jadeiltondm@gmail.com>
2024-01-13 10:23:48 -0300

    feat: create Query in interface ParceiroRepository

--- src/main/java/delivery/api/api/repository/ParceiroRepository.java
+++ src/main/java/delivery/api/api/repository/ParceiroRepository.java
@@ -2,6 +2,13 @@ package delivery.api.api.repository;
 
 import delivery.api.api.domain.Parceiro;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface ParceiroRepository extends MongoRepository<Parceiro,String> {
+
+    @Query("{'document': ?0}")
+    Optional<Parceiro> findPaceiroByDocument(String document);
 }
