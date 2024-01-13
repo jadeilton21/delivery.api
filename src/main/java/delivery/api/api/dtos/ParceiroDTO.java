c572f06537358c0a60ef8c186491a3ec9074e092
delivery.api <jadeiltondm@gmail.com>
2024-01-13 16:47:46 -0300

    feat: create attributes

--- src/main/java/delivery/api/api/dtos/ParceiroDTO.java
+++ src/main/java/delivery/api/api/dtos/ParceiroDTO.java
@@ -1,4 +1,25 @@
 package delivery.api.api.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ParceiroDTO {
+
+    private String id;
+
+    private String tradingName;
+
+    private String ownerName;
+
+    private String document;
+
+    public CoverageAreaDTO coverageArea;
+    public EnderecoDTO enderecoDTO;
+
 }
