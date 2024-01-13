c3d9c71b0bcec8bce73d18506ab5fea0c8120185
delivery.api <jadeiltondm@gmail.com>
2024-01-13 10:30:40 -0300

    feat: create methods in class DocumentoDuplicadoException

--- src/main/java/delivery/api/api/exception/DocumentoDuplicadoException.java
+++ src/main/java/delivery/api/api/exception/DocumentoDuplicadoException.java
@@ -1,4 +1,11 @@
 package delivery.api.api.exception;
 
 public class DocumentoDuplicadoException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+
+    public DocumentoDuplicadoException(String exception){
+        super(exception);
+    }
 }
