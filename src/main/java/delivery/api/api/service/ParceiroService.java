448d6fdc88f242e873fd52d96907538227b5f1fc
delivery.api <jadeiltondm@gmail.com>
2024-01-14 18:46:02 -0300

    feat: create interface ParceiroService

--- src/main/java/delivery/api/api/service/ParceiroService.java
+++ src/main/java/delivery/api/api/service/ParceiroService.java
@@ -1,4 +1,16 @@
 package delivery.api.api.service;
 
+import delivery.api.api.domain.Parceiro;
+import delivery.api.api.dtos.ParceiroDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public interface ParceiroService {
+    ParceiroDTO cadastrar(Parceiro parceiro);
+
+    ParceiroDTO buscarPorId(String id);
+
+    Optional<Parceiro> buscarPorEndereco(double lng, double ltd);
 }
