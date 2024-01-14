0ed7a5733b27ee47a057d5ac6f17d24c612dcd9a
delivery.api <jadeiltondm@gmail.com>
2024-01-14 18:42:33 -0300

    feat: add Service

--- src/main/java/delivery/api/api/resource/ParceiroResource.java
+++ src/main/java/delivery/api/api/resource/ParceiroResource.java
@@ -1,4 +1,12 @@
 package delivery.api.api.resource;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parceiros")
 public class ParceiroResource {
+    @Service
+    private ParceiroService parceiroService;
 }
