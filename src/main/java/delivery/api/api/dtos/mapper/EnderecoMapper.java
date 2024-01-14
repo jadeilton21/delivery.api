3fb920c931a83af862512c4d07f1ce8e6d344898
delivery.api <jadeiltondm@gmail.com>
2024-01-14 19:00:18 -0300

    feat: update class EnderecoMapper

--- src/main/java/delivery/api/api/dtos/mapper/EnderecoMapper.java
+++ src/main/java/delivery/api/api/dtos/mapper/EnderecoMapper.java
@@ -1,4 +1,15 @@
 package delivery.api.api.dtos.mapper;
 
+import delivery.api.api.domain.Endereco;
+import delivery.api.api.dtos.EnderecoDTO;
+
 public class EnderecoMapper {
+
+    public static Endereco toEntity(EnderecoDTO addressDTO) {
+    return new Endereco(addressDTO.getType(), addressDTO.getCordinates());
+}
+
+    public static EnderecoDTO toDTO(Endereco address) {
+        return new EnderecoDTO(address.getType(), address.getCordinates());
+    }
 }
