403e9839cda991d931523dfb0123efd96e1d19fb
delivery.api <jadeiltondm@gmail.com>
2024-01-15 09:34:13 -0300

    feat: create constructor and gets..

--- src/main/java/delivery/api/api/dtos/mapper/CoverageAreaDTO.java
+++ src/main/java/delivery/api/api/dtos/mapper/CoverageAreaDTO.java
@@ -1,4 +1,28 @@
 package delivery.api.api.dtos.mapper;
 
 public class CoverageAreaDTO {
+
+    private String type;
+
+    private double[][][] coordinates;
+
+    public CoverageAreaDTO(){
+
+    }
+
+    public CoverageAreaDTO(String type,double[][][] coordinates){
+        this.type = type;
+        this.coordinates = coordinates;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public double[][][] getCoordinates() {
+        return coordinates;
+    }
+
+
 }
