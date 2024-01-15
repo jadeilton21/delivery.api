cff159769ccfe17cf7286ef00c6889dd8b0e8189
delivery.api <jadeiltondm@gmail.com>
2024-01-15 10:21:56 -0300

    feat: update ParceiroService

--- src/main/java/delivery/api/api/service/ParceiroService.java
+++ src/main/java/delivery/api/api/service/ParceiroService.java
@@ -1,6 +1,8 @@
 package delivery.api.api.service;
 
+import delivery.api.api.domain.Endereco;
 import delivery.api.api.domain.Parceiro;
+import delivery.api.api.dtos.EnderecoDTO;
 import delivery.api.api.dtos.ParceiroDTO;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public interface ParceiroService {
-    ParceiroDTO cadastrar(Parceiro parceiro);
+    EnderecoDTO cadastrar(Endereco parceiro);
 
     ParceiroDTO buscarPorId(String id);
 
