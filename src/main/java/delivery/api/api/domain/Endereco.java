fcef27392c6f31050edee3404837af1636803c7b
delivery.api <jadeiltondm@gmail.com>
2024-01-15 10:21:05 -0300

    feat: update Endereco

--- src/main/java/delivery/api/api/domain/Endereco.java
+++ src/main/java/delivery/api/api/domain/Endereco.java
@@ -1,6 +1,7 @@
 package delivery.api.api.domain;
 
 import com.google.gson.GsonBuilder;
+import com.mapbox.services.commons.geojson.custom.PositionDeserializer;
 import com.mapbox.services.commons.geojson.custom.PositionSerializer;
 import com.mapbox.services.commons.models.Position;
 
@@ -17,12 +18,24 @@ public class Endereco implements Serializable {
         this.type =type;
         this.coordinates= coordinates;
     }
-
+    public static Endereco fromJson(String json) {
+        GsonBuilder gson = new GsonBuilder();
+        gson.registerTypeAdapter(Position.class, new PositionDeserializer());
+        return gson.create().fromJson(json, Endereco.class);
+    }
 
 
     public String toJson(){
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(Position.class,new PositionSerializer());
         return gsonBuilder.create().toJson(this);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double[] getCoordinates() {
+        return coordinates;
     }
 }
