4ee9ee2cb20554471f3f2adf61ae9b644b77657b
delivery.api <jadeiltondm@gmail.com>
2024-01-15 09:46:19 -0300

    feat: create arguments in class

--- src/main/java/delivery/api/api/dtos/mapper/ParceiroDTO.java
+++ src/main/java/delivery/api/api/dtos/mapper/ParceiroDTO.java
@@ -1,4 +1,27 @@
 package delivery.api.api.dtos.mapper;
 
-public class ParceiroDTO {
+import delivery.api.api.domain.Endereco;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ParceiroDTO implements Serializable {
+    private String id;
+
+    private String tradingName;
+
+    private String ownerName;
+
+    private String document;
+
+    public CoverageAreaDTO coverageArea;
+
+    public Endereco address;
 }
